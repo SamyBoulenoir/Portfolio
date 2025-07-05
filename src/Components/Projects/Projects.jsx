1d699db84cc630e5e2e75b1d6e1745a511a78cc5
SamyBoulenoir <74359768+SamyBoulenoir@users.noreply.github.com>
2025-07-05 18:35:39 +0200

    final delivery, adding project

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -4,7 +4,7 @@ import { Image } from 'react-bootstrap';
 const projects = [
   {
     title: 'Mona Éducation',
-    description: 'Développement et maintenance d’une plateforme de cours en ligne (React, Node.js) dans le cadre de ma formation OpenClassrooms.',
+    description: "Mona est une plateforme de recherche de cours en ligne tout en un ! Ce projet m'a permis de d'améliorer mes compétences web, en react.js et node.js, ainsi que de toucher aux github actions et de travailler en collaboration avec différentes équipes.",
     link: 'http://mona-edu.com/',
     img: process.env.PUBLIC_URL + "/Mona.png"
   },
@@ -13,6 +13,12 @@ const projects = [
     description: 'Dépôt de mes projets personnels, expérimentations, contributions open source. Retrouvez tout mon code sur mon GitHub !',
     link: 'https://github.com/SamyBoulenoir',
     img: process.env.PUBLIC_URL + "/Github.webp",
+  },
+    {
+    title: 'Kasa',
+    description: "le projet Kasa m'a donné l'occasion d'implémenter le front-end d'une application de location immobilière en utilisant React et React Router. J'ai ainsi pu créer une expérience utilisateur moderne et réactive, tout en découvrant l'utilisation de Sass pour le style et les animations CSS pour rendre l'application plus attrayante.",
+    link: 'https://github.com/SamyBoulenoir/Kasa',
+    img: process.env.PUBLIC_URL + "/kasa.png",
   },
 ];
 
